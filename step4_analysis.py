aabfbdd2cd184cb6b18ba8af11e552a5768fa885
Dalaska <neozdr@gmail.com>
2015-03-21 23:24:05 -0400

    anaysis

--- step4_analysis.py
+++ step4_analysis.py
@@ -14,7 +14,7 @@ import codecs
 import json
 
 def insert_data(data, db):
-    db.vashon3.insert(data)
+    db.tacoma.insert(data)
 
 def load_data(file_in):
     with open(file_in) as f:
@@ -31,7 +31,7 @@ def get_db():
    
 if __name__ == "__main__": 
     # load data
-    #data = load_data('vashon_cleaned.json')
+    data = load_data('vashon_cleaned.json')
 
     # connect to mongo db
     #db=get_db()
@@ -40,19 +40,21 @@ if __name__ == "__main__":
     #insert_data(data, db)
     
     # quarys
-    print(db.vashon3.count()) # of data
-    print (db.vashon3.find({'type':'way'}).count()) # of way 
-    print (db.vashon3.find({'type':'node'}).count()) # of nodes
-    print (db.vashon3.find({'amenity':'bank'}).count()) # how many banks
+    #print(db.tacoma.count()) # of data
+    #print (db.tacoma.find({'type':'way'}).count()) # of way 
+    #print (db.tacoma.find({'type':'node'}).count()) # of nodes
+    #print (db.tacoma.find({'amenity':'bank'}).count()) # how many banks
     
     # most common source
-    pipeline = [ { "$group" : { "_id" : "$source","count": {"$sum": 1 }}},
-                 { "$sort" : { "count" : -1 }},
-                 #{ "$skip" : 1 },
-                 #{ "$limit" : 1 }
-                 ]
-    result = db.vashon3.aggregate(pipeline)
-    pprint.pprint(result["result"])
+    #pipeline = [ { "$group" : { "_id" : "$source","count": {"$sum": 1 }}},
+    #             { "$sort" : { "count" : -1 }},
+    #             #{ "$skip" : 1 },
+    #             { "$limit" : 10 }
+    #             ]
+    #result = db.tacoma.aggregate(pipeline)
+    #pprint.pprint(result["result"])
+    
+    
     # most common amrity
     #pipeline = [ #{ "$match" : {"$name" : { "$gt":0 }}},
     #            { "$group" : { "_id" : "$amenity","count": {"$sum": 1 }}},
